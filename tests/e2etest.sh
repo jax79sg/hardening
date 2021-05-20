f6281809f2335bfdc2956a7a36784965a59a8618
Kah_Siong Tan <jax79sg@yahoo.com.sg>
2021-05-21 01:13:44 +0800

    update

--- tests/e2etest.sh
+++ tests/e2etest.sh
@@ -1,18 +1,46 @@
-#!/bin/bash
+#/bin/bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+
+FAILEDCOMPONENT=""
+GITLINK="https://github.com/jax79sg/clearml-usage-examples"
+GITFOLDER=${GITLINK##*/}
+#GITFOLDER="hardening"
+UBUNTU1804IMAGE="ubuntu:18.04"
+UBUNTU1604IMAGE="ubuntu:16.04"
+PINGLIST="www.google.com www.yahoo.com"
+DOCKERREPO="docker.io"
+PUSHPULLIMAGE="quay.io/jax79sg/afro-mnt:fake"
+CLEARMLSERVER="mlops.sytes.net:8080"
+KUBEPODDEPLOY="pod-httpd"
+
+### Setting up
+if [ "$(docker images | grep myubuntu:18.04)" != "" ]; then
+   docker rmi -f myubuntu:16.04
+fi
+
+if [ "$(docker images | grep myubuntu:18.04)" != "" ]; then
+   docker rmi -f myubuntu:18.04
+fi
+
+if [ -d ${GITFOLDER} ]; then
+   sudo rm -r ${GITFOLDER}
+fi
+
+shopt -s extdebug
 # exit when any command fails
 set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo -e "\n\"${last_command}\" command filed with exit code $?. \n\nComponents not tested or failed: ${FAILEDCOMPONENTS}\n"' EXIT
+trap 'echo -e "\n${RED}${last_command} command failed. \n\n Components not tested or failed: ${FAILEDCOMPONENTS}${NC} "' EXIT
 
 
 GIT="01.Git"
 APT16="02.APT1604"
 APT18="03.APT1804"
+DOCKERBUILD="DOCKERBUILD"
 DOCKERPUSH="04.DOCKERPUSH"
 DOCKERPULL="05.DOCKERPULL"
 CLEARMLREMOTE="06.CLEARMLREMOTE"
@@ -20,48 +48,121 @@ CLEARMLLOCAL="07.CLEARMLLOCAL"
 CLEARMLUI="08.CLEARMLUI"
 PIPINSTALL="09.PIPINSTALL"
 PIPUPDATE="10.PIPUPDATE"
-KUBEPOD="11.KUBE GET PODS"
-KUBECREATE="12.KUBE CREATE"
+KUBENODE="11.KUBEGETNODES"
+KUBEPOD="11.KUBEGETPODS"
+KUBECREATE="12.KUBECREATE"
+PINGTEST="PING"
 
-FAILEDCOMPONENTS="${GIT} ${APT16} ${APT18} ${DOCKERPUSH} ${DOCKERPULL} ${CLEARMLREMOTE} ${CLEARMLLOCAL} ${CLEARMLUI} ${PIPINSTALL} ${PIPUPDATE} ${KUBEPOD} ${KUBECREATE}"
+FAILEDCOMPONENTS="${PINGTEST} ${GIT} ${APT16} ${APT18} ${DOCKERPUSH} ${DOCKERPULL} ${CLEARMLREMOTE}  ${CLEARMLUI} ${PIPINSTALL} ${KUBENODE} ${KUBEPOD} ${KUBECREATE} ${DOCKERBUILD}"
 
 success () {
+  FAILEDCOMPONENTS=${FAILEDCOMPONENTS//$1/}
   echo -e "${GREEN}\nSUCCESS!: $1 \n${NC}"
-  FAILEDCOMPONENTS=${FAILEDCOMPONENTS#"$1"}
+  echo -e "${RED}Components not tested or failed: ${FAILEDCOMPONENTS}${NC}\n"
+}
+
+pingtest () {
+ echo -e "\n\nPerforming PING test for $1"
+ for i in $1
+ do 
+   ping -c 2 $i
+ done
+ success ${PINGTEST}
 }
 
 gittest () {
-  git clone $1
-  foo=$1
-  folder=${foo##*/}
-  cd ${folder}
-  touch test.tst
-  git add test.tst
-  git commit -a -m "test"
-  git push   
-  rm test.txt
-  git commit -a -m "test"
-  git push
+  echo -e "\n\nPerforming ${GIT} test"
+  docker run --rm myubuntu:18.04 /bin/bash -c "apt update && apt install -y git && git clone $1"
   success ${GIT}
 }
 
-#git clone http://gitlab.dsta.ai/aiplatform/clienttest
-gittest  https://github.com/jax79sg/clearml-usage-examples
-#cd clienttest
-#cd clearml-usage-examples
-#success ${GIT}
-echo "--> Building and pushing docker images"
-docker build -t harbor.dsta.ai/public/ubuntu:dstaai.16.04 -f Dockerfile1604 .
-FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, APT 16.04"
-docker push harbor.dsta.ai/public/ubuntu:dstaai.16.04
-FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, DOCKER PUSH"
-docker build -t harbor.dsta.ai/public/ubuntu:dstaai.18.04 -f Dockerfile1804 .
-FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, APT 18.04"
-docker push harbor.dsta.ai/public/ubuntu:dstaai.18.04
-docker rmi -f harbor.dsta.ai/public/ubuntu:dstaai.18.04
-docker pull harbor.dsta.ai/public/ubuntu:dstaai.18.04
-fi
-echo TESTING APT
+dockertest () {
+  if [ "$1" = "${DOCKERBUILD}" ]; then
+     echo -e "\n\nPerforming $1 test"
+     docker build -f Dockerfile.1804 -t myubuntu:18.04 .
+     docker build -f Dockerfile.1604 -t myubuntu:16.04 .
+     success $1
+  elif [ "$1" = "${DOCKERPUSH}" ]; then
+     echo -e "\n\nPerforming $1 test"
+     docker build -f Dockerfile.1804 -t ${PUSHPULLIMAGE} .
+     docker login ${DOCKERREPO}
+     docker push ${PUSHPULLIMAGE}
+     success $1
+  elif [ "$1" = "${DOCKERPULL}" ]; then
+     echo -e "\n\nPerforming $1 test"
+     docker pull ${PUSHPULLIMAGE}
+     success $1
+  else
+     echo -e "\n${RED}DOCKER TEST $1 DID NOT PROCEED AS EXPECTED!${NC}\n"
+  fi
+}
+
+apttest () {
+  if [ "$1" = "${APT18}" ]; then
+    echo -e "\n\nPerforming $1 test"
+    docker run --rm myubuntu:18.04 /bin/bash -c "apt update && apt install -y vim"
+    success $1
+  elif [ "$1" = "${APT16}" ]; then
+    echo -e "\n\nPerforming $1 test"
+    docker run --rm myubuntu:16.04 /bin/bash -c "apt update && apt install -y vim"
+    success $1
+  fi
+}
+
+piptest () {
+  if [ -d ./venv ]; then
+    sudo rm -r venv
+  fi
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install --upgrade pip
+  success ${PIPINSTALL}
+}
+
+kubetest () {
+  if [ "$1" = "${KUBENODE}" ]; then
+    kubectl get nodes
+    success $1
+  elif [ "$1" = "${KUBEPOD}" ]; then
+    if [[ "$(kubectl get pods)" == *"pod-httpd"* ]]; then
+      success $1
+    fi
+  elif [ "$1" = "${KUBECREATE}" ]; then
+    if [[ "$(kubectl get pods)" == *"pod-httpd"* ]]; then
+      kubectl delete -f simple.yaml
+    fi
+    kubectl create -f simple.yaml 
+    success $1
+  fi
+}
 
 
+clearmltest () {
+  if [ "$1" = "${CLEARMLUI}" ]; then
+    HTMLOUTPUT="$(curl ${CLEARMLSERVER} | grep ClearML)"
+    if [ ${HTMLOUTPUT} = "<title>ClearML</title>" ]; then
+       success $1
+    fi
+  elif [ "$1" = "${CLEARMLREMOTE}" ]; then
+    docker run myubuntu:18.04 /bin/bash -c "apt update && apt install -y git python3-dev python3-venv && git clone ${GITLINK} && cd ${GITFOLDER}/detectron2/codes && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install setuptools wheel clearml && python runremotetrain.py"
+    echo "CHECK ${CLEARMLSERVER} FOR RUNNING TASK FOR RUNNING TASK"
+    success $1
+  elif [ "$1" = "${CLEARMLLOCAL}" ]; then 
+    echo "To implement" 
+  fi
+}
 
+
+pingtest ${PINGLIST}
+dockertest ${DOCKERBUILD}
+dockertest ${DOCKERPUSH}
+dockertest ${DOCKERPULL}
+apttest ${APT16}
+apttest ${APT18}
+gittest ${GITLINK}
+piptest 
+clearmltest ${CLEARMLUI}
+kubetest ${KUBENODE}
+kubetest ${KUBECREATE}
+kubetest ${KUBEPOD}
+clearmltest ${CLEARMLREMOTE}
