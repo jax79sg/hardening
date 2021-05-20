cc1595d69617c692a64425eb42be9901dd355fd8
Kah_Siong Tan <jax79sg@yahoo.com.sg>
2021-05-20 15:47:18 +0800

    test

--- tests/e2etest.sh
+++ tests/e2etest.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 # exit when any command fails
 set -e
-PASSEDCOMPONENTS="01.Git, 02.APT16,04, 03.APT18.04, 04.DOCKERPUSH, 05.DOCKERPULL"
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo -e "\"${last_command}\" command filed with exit code $?. \n     PASSEDCOMPONENTS: ${PASSEDCOMPONENTS}"' EXIT
+trap 'echo -e "\n\"${last_command}\" command filed with exit code $?. \n\nComponents not tested or failed: ${FAILEDCOMPONENTS}\n"' EXIT
 
 
 GIT="01.Git"
@@ -21,24 +23,40 @@ PIPUPDATE="10.PIPUPDATE"
 KUBEPOD="11.KUBE GET PODS"
 KUBECREATE="12.KUBE CREATE"
 
+FAILEDCOMPONENTS="${GIT} ${APT16} ${APT18} ${DOCKERPUSH} ${DOCKERPULL} ${CLEARMLREMOTE} ${CLEARMLLOCAL} ${CLEARMLUI} ${PIPINSTALL} ${PIPUPDATE} ${KUBEPOD} ${KUBECREATE}"
 
 success () {
-  PASSEDCOMPONENTS="${PASSEDCOMPONENTS}, $1"
+  echo -e "${GREEN}\nSUCCESS!: $1 \n${NC}"
+  FAILEDCOMPONENTS=${FAILEDCOMPONENTS#"$1"}
 }
 
-echo "--> Pulling the dependancies for tesing"
+gittest () {
+  git clone $1
+  foo=$1
+  folder=${foo##*/}
+  cd ${folder}
+  touch test.tst
+  git add test.tst
+  git commit -a -m "test"
+  git push   
+  rm test.txt
+  git commit -a -m "test"
+  git push
+  success ${GIT}
+}
+
 #git clone http://gitlab.dsta.ai/aiplatform/clienttest
-git clone https://github.com/jax79sg/clearml-usage-examples
+gittest  https://github.com/jax79sg/clearml-usage-examples
 #cd clienttest
-cd clearml-usage-examples
-success "GIT"
+#cd clearml-usage-examples
+#success ${GIT}
 echo "--> Building and pushing docker images"
 docker build -t harbor.dsta.ai/public/ubuntu:dstaai.16.04 -f Dockerfile1604 .
-PASSEDCOMPONENTS="${PASSEDCOMPONENTS}, APT 16.04"
+FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, APT 16.04"
 docker push harbor.dsta.ai/public/ubuntu:dstaai.16.04
-PASSEDCOMPONENTS="${PASSEDCOMPONENTS}, DOCKER PUSH"
+FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, DOCKER PUSH"
 docker build -t harbor.dsta.ai/public/ubuntu:dstaai.18.04 -f Dockerfile1804 .
-PASSEDCOMPONENTS="${PASSEDCOMPONENTS}, APT 18.04"
+FAILEDCOMPONENTS="${FAILEDCOMPONENTS}, APT 18.04"
 docker push harbor.dsta.ai/public/ubuntu:dstaai.18.04
 docker rmi -f harbor.dsta.ai/public/ubuntu:dstaai.18.04
 docker pull harbor.dsta.ai/public/ubuntu:dstaai.18.04
